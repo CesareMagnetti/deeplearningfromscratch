d8de95b204e01c891b0d6f27788f28d2c1a65766
cesare <cesare.magnetti98@gmail.com>
2021-01-16 17:34:34 +0000

    working on convolutional forward pass...

--- modules.py
+++ modules.py
@@ -20,6 +20,9 @@ class Module():
     def forward(self, *args, **kwargs):
         raise NotImplementedError('forward function not yet implemented!')
 
+    def bwd(self, *args, **kwargs):
+        raise NotImplementedError('bwd (backward implementation) method not yet implemented!'
+                                  )
     def backward(self):
         return self.bwd(self.out)
 
@@ -36,7 +39,8 @@ class Linear(Module):
     def __init__(self, in_features: int, out_features: int, store_grad:bool = False):
         super(Linear, self).__init__()
         self.weights.set_data(np.random.rand(in_features, out_features), store_grad)
-        self.bias.set_data(np.random.rand(out_features), store_grad)
+        # bias is initialised to zero
+        self.bias.set_data(np.zeros(out_features), store_grad)
 
     def forward(self, inputs: np.ndarray) -> np.ndarray:
         if len(inputs.shape)>1:
@@ -50,29 +54,23 @@ class Linear(Module):
 
 
 class Conv2D(Module):
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int = 1,
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: tuple, stride: int = 1,
                  padding: int = 0, bias:bool = True, store_grad:bool = False):
 
         super(Conv2D, self).__init__()
 
-        self.weights.set_data(np.random.rand(out_channels, in_channels, kernel_size, kernel_size), store_grad)
+        self.weights.set_data(np.random.rand(out_channels, in_channels, *kernel_size), store_grad)
         if bias:
-            self.bias.set_data(np.random.rand(out_channels), store_grad)
+            # bias initialised to zero
+            self.bias.set_data(np.zeros(out_channels), store_grad)
 
         self.ks, self.stride, self.pad = kernel_size, stride, padding
 
     def forward(self, input:np.ndarray) -> np.ndarray:
 
         # convolve input
-        if self.bias:
-            return correlation2d(input, self.weights.data, self.ks, self.stride, self.pad) + self.bias
+        if self.bias.data is not None:
+            return correlation2d(input, self.weights.data, stride=self.stride, padding=self.pad) + self.bias.data
         else:
-            return correlation2d(input, self.weights.data, self.ks, self.stride, self.pad)
+            return correlation2d(input, self.weights.data,stride=self.stride, padding=self.pad)
 
-    def backward(self):
-        # return self.bwd(self.out)
-        raise NotImplementedError('Not implemented yet')
-
-    def __call__(self, inputs):
-        self.out = self.forward(inputs)
-        return self.out
--- utils.py
+++ utils.py
@@ -1,66 +1,78 @@
 import numpy as np
 import scipy as sp
 
-def _correlation2d(input:np.ndarray, filter:np.ndarray, stride:int = 1, dil:int = 0, pad:int = 0) -> np.ndarray:
+def correlation2d(input:np.ndarray, kernel:np.ndarray, stride:int = 1, padding:int = 0) -> np.ndarray:
     '''
     basic 2d correlation operator in1*in2. simply move throughout in1 and evaluate the local dot product with in2.
 
 
-    :param in1: 3D ndarray, second input for the correlation operator. shape: CxHxW.
-    :param filter: 3D ndarray, second input for the correlation operator. shape: CxHxW.
-    :param stride: int, how many entries to skip (both horizontally and vertically) when moving in2 throughout in1.
+    :param in1: 4D ndarray, second input for the correlation operator. shape: BxCxHxW.
+    :param kernel: 4D ndarray, second input for the correlation operator. shape: BxCxHxW.
+    :param stride: int, how many entries to skip (both horizontally and vertically) when moving filter throughout input.
                         (default: 1)
-    :param dil: int, spacing between elements of in2.
-                        (default: 0)
-    :param pad: int, amount of zero pad for in1.
+    :param padding: int, amount of zero pad for input.
                         (default: 0)
     :return:
     '''
 
-    c_in, h_in, w_in = input.shape
-    cf, hf, wf = filter.shape
+    B,C,H,W = input.shape # B is the batch size (number of inputs)
+    Nk,Ck,Hk,Wk = kernel.shape # Nk is the number of kernels used. EACH OF THE B INPUTs GOES THROUGH ALL Nk KERNELS.
 
-    assert c_in == cf, "ERROR: input and filters must have the same number of channels!\n" \
-                     "input shape: {} filter shape: {}".format(input.shape, filter.shape)
+    # 1 kernel and input must have the same number of channels
+    assert C == Ck, "ERROR: input and filters must have the same number of channels!"
 
-    if not (hf<=h_in or wf<=w_in):
-        Warning('filter dimesions are larger than input! may lead to unexpected behaviour.\n'
-                'input shape: {} filter shape: {}'.format(input.shape, filter.shape))
+    if not (Hk<=H or Wk<=W):
+        Warning('kernel size is larger than the input! may lead to unexpected behaviour.\n'
+                'input shape: {} filter shape: {}'.format(input.shape[-2:], kernel.shape[-2:]))
 
-    # out_w = int((w_in + 2 * pad - dil * (wf - 1) - 1)/stride) + 1
-    # out_h = int((h_in + 2 * pad - dil * (hf - 1) - 1)/stride) + 1
+    # 2 get output dimensions
+    Hout = int((H - Hk + 2 * padding)/stride) + 1
+    Wout = int((W - Wk + 2 * padding)/stride) + 1
 
+
+    # 3 pad image if necessary
     # np.pad defaults to zero padding, do not pad in the channnel dimension
-    if pad>0:
-        input = np.pad(input, ((0,0), (pad,pad), (pad, pad)))
+    if padding>0:
+        input = np.pad(input, ((0,0), (0,0), (padding,padding), (padding, padding)))
 
+    # 4 reshape kernels from Nk x Ck x Hk x Wk to Nk x Ck*Hk*Wk
+    kernel = kernel.reshape(Nk, -1)
 
-def correlation2d(inputs:np.ndarray, kernel:np.ndarray, kwargs) -> np.ndarray:
-    '''
-    handle for _correlation2d() which computes the correlation between two numpy arrays.
+    # 5 transform input to a matrix whose columns are the neighborhood of the kernel at each position. Since inputs are
+    # processed in batches we will stack B of such matrices together to achieve a shape of B x (Ck*Hk*Wk) x (H*W).
+    # Note that this is not really a reshape because elements are repeated.
+    # for a good overview of this concept see publication:
+    # https://www.researchgate.net/publication/330315719_A_Uniform_Architecture_Design_for_Accelerating_2D_and_3D_CNNs_on_FPGAs
 
+    transformed_input = np.zeros((B, (Ck*Hk*Wk), H*W))
+    
 
-    :param inputs: ndarray 3-4D, input(s) for the correlation operator.
-    :param kernel: ndarray 4D, kernel weights of the conv layer. 4D because many filters are used in each layer.
-    :param kwargs: keyworded additional parameters to pass to _correlation2d.
-
-
-    :return: ndarray
-    '''
-
-    if len(inputs.shape)>3:
-        output = []
-        for _input in inputs:
-            f_maps = []
-            for filter in kernel:
-                f_maps.append(_correlation2d(_input, filter, **kwargs))
-            output.append(np.stack(f_maps))
-    else:
-        output = []
-        for filter in kernel:
-            output.append(_correlation2d(inputs, filter, **kwargs))
-
-    return np.stack(output)
+# def correlation2d(inputs:np.ndarray, kernel:np.ndarray, **kwargs) -> np.ndarray:
+#     '''
+#     handle for _correlation2d() which computes the correlation between two numpy arrays.
+#
+#
+#     :param inputs: ndarray 3-4D, input(s) for the correlation operator.
+#     :param kernel: ndarray 4D, kernel weights of the conv layer. 4D because many filters are used in each layer.
+#     :param kwargs: keyworded additional parameters to pass to _correlation2d.
+#
+#
+#     :return: ndarray
+#     '''
+#
+#     if len(inputs.shape)>3:
+#         output = []
+#         for _input in inputs:
+#             f_maps = []
+#             for filter in kernel:
+#                 f_maps.append(_correlation2d(_input, filter, **kwargs))
+#             output.append(np.stack(f_maps))
+#     else:
+#         output = []
+#         for filter in kernel:
+#             output.append(_correlation2d(inputs, filter, **kwargs))
+#
+#     return np.stack(output)
 
 
 def _relu(input):
