345d67a2ea21e2d94bc886e1a17e85bb3924ae95
cesare <cesare.magnetti98@gmail.com>
2021-04-05 23:48:19 +0100

    updating code

--- modules.py
+++ modules.py
@@ -1,92 +1,228 @@
 import utils
 import numpy as np
+from collections import OrderedDict
 
 class Module():
+    """
+    Base class that all instanciated model/layer must inherit, it will set up the model/layer and
+    handle basic functionalities.
+    """
+
     def __init__(self):
-        self.weights = None
-        self.bias = None
+        self.training = False # boolean variable to represent id module in training mode or evaluation mode
+        self._parameters = OrderedDict() # store module's parameters
+        self._caches = OrderedDict() # store forward pass caches to compute derivatives in backward pass
+        self._backward_hooks = OrderedDict() # store the grad of the parameters when backward is called
+        self._modules = OrderedDict() # store all sub-modules that will be contained in the model
 
     def forward(self, *args, **kwargs):
-        raise NotImplementedError('forward function not yet implemented!')
+        raise NotImplementedError('forward function not yet implemented for testing!')
+
 
     def bwd(self, *args, **kwargs):
-        raise NotImplementedError('bwd (backward implementation) method not yet implemented!'
-                                  )
-    def backward(self, dout):
-        return self.bwd(dout, self.cache)
+        pass
+
+    def backward(self, Dout = None):
+        return self.bwd(Dout)
 
     def __call__(self, *args, **kwargs):
-        out, self.cache = self.forward(*args, **kwargs)
-        return out.transpose(3,0,1,2) # due to how we performed forward pass
+        return self.forward(*args, **kwargs)
 
+    def add_parameter(self, name:str, param:np.ndarray) -> None:
+        if name == '':
+            raise ValueError('parameter name cannot be empty')
+        else:
+            assert isinstance(param, np.ndarray), "can only add instances of ``ndarray`` to self._parameters. Got {}".format(type(param))
+            self._parameters[name] = param
+
+    def add_cache(self, name:str, param:np.ndarray) -> None:
+        if name == '':
+            raise ValueError('cache name cannot be empty')
+        else:
+            assert isinstance(param, np.ndarray), "can only add instances of ``ndarray`` to self._caches. Got {}".format(type(param))
+            self._caches[name] = param
+
+    def add_module(self, name:str, module) -> None:
+        if name == '':
+            raise ValueError('module name cannot be empty')
+        else:
+            assert isinstance(module, Module), "can only add modules that inherit the ``Module`` class. Got {}".format(type(module))
+            self._modules[name] = module
+
+
+    def add_backward_hook(self, name:str, param:np.ndarray) -> None:
+        if name == '':
+            raise ValueError('backward hook name cannot be empty')
+        else:
+            assert isinstance(param, np.ndarray), "can only add instances of ``ndarray`` to self._backward_hooks. Got {}".format(type(param))
+            self._backward_hooks[name] = param
+
+    def update_params(self, Dout=None, lr = 0.001):
+        """
+        updates parameters using gradient descent
+        :param loss: Module, loss class after a forward pass
+        :param Dout: np.ndarray, current downstream gradient
+        :param lr: learning rate, default 0.001
+        """
+        if self.training:
+            if self._modules:
+                for name in reversed(self._modules):
+                    module = self._modules[name]
+                    Dout = module.backward(Dout)
+                    for param, hook in module._backward_hooks.items():
+                        module._parameters[param] -= lr*hook
+            else:
+                _ = self.backward(Dout)
+                for param, hook in self._backward_hooks.items():
+                    self._parameters[param] -= lr * hook
+
+    def zero_grad(self):
+        self._caches = OrderedDict()
+        self._backward_hooks = OrderedDict()
+
+
+    def train(self):
+        self.training = True
+
+    def eval(self):
+        self.training = False
+
+    def named_parameters(self):
+        if self._modules:
+            return self.submodules()
+        else:
+            return self._parameters.items()
+
+    def submodules(self):
+        return self._modules.items()
+
+    def reversed_submodules(self):
+        reversed_modules = OrderedDict()
+        for name in  reversed(self._modules):
+            reversed_modules[name] = self._modules[name]
+        return reversed_modules.items()
+
+    def extra_repr(self):
+        _name = "\n"
+        if self._modules:
+            for _, module in self.submodules():
+                for key, item in module.named_parameters():
+                    _name += "\t{}\t{}\n".format(key, item.shape)
+                _name = "\n"
+        else:
+            for key, item in self.named_parameters():
+                _name += "\t{}\t{}\n".format(key, item.shape)
+
+        return _name
 
 
 class Linear(Module):
     '''
-    implementation of a linear layer which simply implements W*input + bias and evaluates gradients once called pass
+    implementation of a linear layer and its backward
     '''
-    def __init__(self, in_features: int, out_features: int, store_grad:bool = False):
+
+    def __repr__(self):
+        _name = "Linear layer"
+        return _name+self.extra_repr()
+
+    def __init__(self, in_features: int, out_features: int):
         super(Linear, self).__init__()
-        self.weights = np.random.rand(in_features, out_features)
+        # weights initialised from a centralized random distribution with std 0.1
+        self.add_parameter("weight", np.random.normal(0, 0.1, size=(in_features, out_features)))
         # bias is initialised to zero
-        self.bias = np.zeros(out_features)
+        self.add_parameter("bias", np.zeros(out_features))
 
-    def forward(self, inputs: np.ndarray) -> tuple:
-        out = inputs @ self.weights + self.bias
-        cache = inputs, self.weights
-        return (out, cache)
+    def forward(self, input: np.ndarray) -> tuple:
+        weight, bias = self._parameters['weight'], self._parameters['bias']
+        out = input @ weight + bias
+        self.add_cache("input", input)
+        return out
 
-    def bwd(self, Dout:np.ndarray, cache:tuple) -> tuple:
+    def bwd(self, Dout:np.ndarray) -> np.ndarray:
         '''
         function to perform backward pass on linear layer
-        :param Dout: np.ndarray, differential on the output of the input layer. shape: BxN
-        :param cache: tuple, containes cached variables during forward pass for easy differentiation
-        :return: (dW, db, dX) tuple of partial derivatives wrt to parameters W (weights), b (bias) and input X
+        :param Dout: np.ndarray, differential on the output w.r.t. input layer.
+        :return: dX: np.ndarray, gradient wrt to the input of the layer, will be used by previous layers as
+                     Dout (input for backwards). DW and Db will be stored in the _backward_hooks attribute,
+                     which will update the layers parameters when needed.
         '''
-        input, W = cache
 
         # partials are trivial since linear layer simply performs XW+b, note that b is broadcasted element-wise and that
         # this can be a general inner layer, hence derivatives depend on Dout due to chain rule.
+        input = self._caches['input']
         DW = input.T @ Dout #d[XW+b}/dW = X then inner product with Dout due to chain rule.
         Db = np.sum(Dout, axis=0) #d[XW+b]/db = 1 then multiply by Dout due to chain rule.
                                   # sum over first dimension because b was broadcasted element-wise
-        DX = Dout @ W.T #d[XW+b}/dX = W then inner product with Dout due to chain rule.
 
-        return (DW, Db, DX)
+        weight, bias = self._parameters['weight'], self._parameters['bias']
+        DX = Dout @ weight.T #d[XW+b}/dX = W then inner product with Dout due to chain rule.
+
+        # store gradients of the layers parameters
+        self.add_backward_hook("weight", DW)
+        self.add_backward_hook("bias", Db)
+
+        return DX
 
 
 
 class Conv2D(Module):
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: tuple, stride: int = 1,
-                 padding: int = 0, bias:bool = True, store_grad:bool = False):
+    """
+    An implementation of a convolutional layer.
+
+    The input consists of B data points, each with C channels, height H and
+    width W. We convolve each input with Nk different filters, where each filter
+    spans all C channels and has height Hk and width Wk.
+
+    Parameters:
+    - w: Filter weights of shape (Nk, C, Hk, Wk)
+    - b: Biases, of shape (Nk,)
+    - kernel_size: Size of the convolving kernel
+    - stride: The number of pixels between adjacent receptive fields in the
+        horizontal and vertical directions.
+    - padding: The number of pixels that will be used to zero-pad the input.
+    """
+
+    def __repr__(self):
+        _name = "Conv2D layer"
+        return _name+self.extra_repr()
+
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: tuple, stride: int = 1, padding: int = 0):
 
         super(Conv2D, self).__init__()
 
-        self.weights = np.random.rand(out_channels, in_channels, *kernel_size)
-        if bias:
-            # bias initialised to zero
-            self.bias = np.zeros((out_channels,1))
+        # weights initialised from a centralised random distribution with std 0.1
+        self.add_parameter("weight", np.random.normal(0, 0.1, size=(out_channels, in_channels, *kernel_size)))
+        # bias is initialised to zero
+        self.add_parameter("bias", np.zeros((out_channels,1)))
 
         self.ks, self.stride, self.pad = kernel_size, stride, padding
 
     def forward(self, input:np.ndarray) -> tuple:
+        '''
+        see comments of utils.correlation2d() for insights on the forward pass of conv2D
+        :param input: np.ndarray, input to the conv2D layer
+        :return: out: np.ndarray, output to the conv2D layer
+        '''
+        weight, bias = self._parameters['weight'], self._parameters['bias']
         # forward pass for conv layer and stores cached variables for backward pass
-        return  utils.correlation2d(input, self.weights, self.bias,
-                                    stride=self.stride, padding=self.pad)
+        out, caches = utils.correlation2d(input, weight, bias, stride=self.stride, padding=self.pad)
+        for key, cache in caches.items():
+            self.add_cache(key, cache)
+        return out
 
-    def bwd(self, Dout:np.ndarray, cache:tuple) -> tuple:
+    def bwd(self, Dout:np.ndarray) -> np.ndarray:
         '''
         function to perform backward pass on conv layer
         :param Dout: np.ndarray, differential on the output of the conv layer. shape: NkxCxHxW
-        :param cache: tuple, containes cached variables during forward pass for easy differentiation
-        :return: (dW, db, dX) tuple of partial derivatives wrt to parameters W (kernel weights), b (bias) and input X
+        :return: dX: np.ndarray, gradient wrt to the input of the layer, will be used by previous layers as
+                     Dout (input for backwards). DW and Db will be stored in the layer parameters .grad attribute,
+                     which will update the layers parameters when needed.
         '''
 
-        input, transformed_input, kernel, bias, stride, padding = cache
+        input, transformed_input = self._caches['input'], self._caches['transformed_input']
+        weight, bias = self._parameters['weight'], self._parameters['bias']
 
-        # get shapes
-        B, C, H, W = input.shape  # B is the batch size (number of inputs)
-        Nk, Ck, Hk, Wk = kernel.shape  # Nk is the number of kernels used.
+        Nk, Ck, Hk, Wk = weight.shape  # Nk is the number of kernels used.
 
         # gradients that have to be calculated in a con layer:
         #   - Db: Dout*dout/db partial derivative of out w.r.t. bias
@@ -110,7 +246,7 @@ class Conv2D(Module):
         # need to reshape dout, which is assumed to have standard image shape NkxCxHxW to the same shape it has in
         # utils.correlation2D before being reshaped. We also the kernel for the same reasons.
         Dout = Dout.transpose(1,2,3,0).reshape(Nk,-1)
-        kernel = kernel.reshape(Nk,-1)
+        kernel = weight.reshape(Nk,-1)
 
         # partial of weights will be the input itself, which we multiply by Dout by chain rule
         DW = Dout @ transformed_input.T
@@ -121,9 +257,156 @@ class Conv2D(Module):
 
         # we now need to do a reshaping procedure opposite to the one we did in utils.correlation2D, to get the image back
         # to its original shape
-        DX = utils.neighborhoodMatrix2image(tranformed_DX, input.shape, kernel.shape, stride, padding)
+        DX = utils.neighborhoodMatrix2image(tranformed_DX, input.shape, weight.shape, self.stride, self.pad)
 
-        return (DW, Db, DX)
+        # store gradients of the layers parameters
+        self.add_backward_hook("weight", DW)
+        self.add_backward_hook("bias", Db)
+
+        return DX
+
+class View(Module):
+    """
+    class to reshape an input to go through linear layers and include it in backprop
+    """
+    def __repr__(self):
+        return "View tensor\n"
+
+    def __init__(self, shape):
+        super(View, self).__init__()
+        self.shape = shape
+
+    def forward(self, input):
+        reshaped_input = input.reshape(self.shape)
+        self.add_cache("input", input)
+        self.add_cache("reshaped_input", reshaped_input)
+        return reshaped_input
+
+    def bwd(self, Dout):
+        input, reshaped_input = self._caches["input"], self._caches["reshaped_input"]
+        Dout = Dout.reshape(input.shape)
+        return Dout
+
+class ReLU(Module):
+    """
+    rectified linear unit activation function: out = max(0,X)
+    """
+
+    def __repr__(self):
+        _name = "ReLU activation\n"
+        return _name
+
+    def __init__(self):
+        super(ReLU, self).__init__()
+
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        mask = input <= 0
+        self.add_cache("mask", mask)
+        input[mask] = 0
+        return input
+
+    def bwd(self, Dout):
+        mask = self._caches["mask"]
+        DX = Dout.copy()
+        DX[mask] = 0
+        return DX
 
 
+class Sigmoid(Module):
+    """
+    sigmoid activation function: out = 1/[1+exp(-X)]
+    """
+
+    def __repr__(self):
+        _name = "Sigmoid activation\n"
+        return _name
+
+    def __init__(self):
+        super(Sigmoid, self).__init__()
+
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        '''
+        aplying sigmoid forward pass (storing necessary caches)
+        :param input: np.ndarray, input to the softmax layer
+        :return: out: np.ndarray, output to the softmax layer
+        '''
+        out = 1/(1+np.exp(-input))
+        self.add_cache("out", out)
+        return out
+
+    def bwd(self, Dout):
+        out = self._caches["out"]
+        return Dout*out*(1-out)
+
+class Softmax(Module):
+    """
+    sigmoid activation function: out_j = exp(x_j)/[sum_i (exp(-x_i)]
+    """
+
+    def __repr__(self):
+        _name = "Softmax activation\n"
+        return _name
+
+    def __init__(self):
+        super(Softmax, self).__init__()
+
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        '''
+        aplying softmax forward pass (storing necessary caches)
+        :param input: np.ndarray, input to the softmax layer
+        :return: out: np.ndarray, output to the softmax layer
+        '''
+        e = np.exp(input - np.max(input))
+        s = np.sum(e, axis=1, keepdims=True)
+        softmax = e / s
+        # cache variables for backward pass
+        self.add_cache("input", input)
+        self.add_cache("softmax", softmax)
+        return softmax
+
+    def bwd(self, Dout):
+        # the derivative of a softmax can be derived to be dS(x)/dx = Sj*(del_jk - Sk), where S(x) is the softmax of x
+        # and del_jk is the kronecker delta function.
+
+        input, softmax = self._caches["input"], self._caches["softmax"]
+        B,N = input.shape
+
+        # expand above equation to Sj*del_ij - Sj*Sk, where both Sj and Sk are 2D matrices BxN where N is the number of neurons.
+        # Sj*Sk is an outer product of S with itself
+        Sj_Sk = np.einsum('ij,ik->ijk', softmax, softmax)  # BxNxN
+
+        #Sj*del_jk
+        Sj_deljk = np.einsum('ij,jk->ijk', softmax, np.eye(N, N))  # BxNxN
+
+        dSoftmax = Sj_deljk - Sj_Sk
+
+        # we multiply the derivative of the softmax w.r.t input by downstream gradient Dout (chain rule)
+        Dout = np.einsum('ijk,ik->ij', dSoftmax, Dout)  # BxN
+        return Dout
+
+class NegativeLogLikelyhood(Module):
+    """
+    Negative Log Likelyhood Loss for single class MLE fit, assuming labels as integers starting from 0
+    """
+
+    def __repr__(self):
+        return "Negative Log-Likelyhood loss\n"
+
+    def __init__(self):
+        super(NegativeLogLikelyhood, self).__init__()
+
+    def forward(self, input, label):
+        B = input.shape[0]
+        # approximate full loss with batch loss (SGD)
+        NLL = -np.mean(np.log(input[np.arange(B), label]))
+        self.add_cache("input", input)
+        self.add_cache("label", label)
+        return NLL
+
+    def bwd(self, Dout=None):
+        input, label = self._caches["input"], self._caches["label"]
+        B = input.shape[0]
+        Dout = np.zeros(input.shape)
+        Dout[np.arange(B), label] = -1/input[np.arange(B), label]
+        return Dout
 
--- utils.py
+++ utils.py
@@ -2,14 +2,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def show_tensor(tensor):
-    plt.imshow(tensor.transpose(1, 2, 0))
+def show_tensor(tensor, normalize = False, cmap = "gray"):
+    if normalize:
+        tensor /= tensor.max()
+    plt.imshow(tensor.transpose(1, 2, 0), cmap=cmap)
     plt.title("original image")
     plt.axis("off")
     plt.show()
 
 
-def show_batch_of_tensors(batch, ncol=5):
+def show_batch_of_tensors(batch, ncol=5, normalize = False, cmap = "gray"):
     B, C, H, W = batch.shape
     extra = 0
     if not B % ncol == 0:
@@ -19,7 +21,9 @@ def show_batch_of_tensors(batch, ncol=5):
     fig, axs = plt.subplots(int(B / ncol) + extra, ncol)
 
     for (tensor, ax) in zip(batch, axs.ravel()[:B]):
-        ax.imshow(tensor.transpose(1, 2, 0))
+        if normalize:
+            tensor /= tensor.max()
+        ax.imshow(tensor.transpose(1, 2, 0), cmap=cmap)
         ax.axis("off")
 
     plt.tight_layout()
@@ -61,7 +65,7 @@ def get_neighborhood_indeces(input_shape:tuple, kernel_shape:tuple, stride:int, 
 
     return (ii, jj, kk)
 
-def image2neighbourhoodMatrix(input: np.ndarray, kernel_shape:tuple, stride:int, padding:int)-> tuple:
+def image2neighbourhoodMatrix(input: np.ndarray, kernel_shape:tuple, stride:int, padding:int)-> np.ndarray:
 
     '''
     # transform input to a matrix whose columns are the neighborhood of the kernel at each position. Since inputs are
@@ -83,8 +87,13 @@ def image2neighbourhoodMatrix(input: np.ndarray, kernel_shape:tuple, stride:int,
     B,C,H,W = input.shape # B is the batch size (number of inputs)
     Nk,Ck,Hk,Wk = kernel_shape # Nk is the number of kernels used. EACH OF THE B INPUTs GOES THROUGH ALL Nk KERNELS.
 
+    # pad image if necessary
+    # np.pad defaults to zero padding, do not pad in the channnel dimension
+    if padding>0:
+        input = np.pad(input, ((0,0), (0,0), (padding,padding), (padding, padding)))
+
     # get indeces of all possible neighours
-    ii, jj, kk = get_neighborhood_indeces(input.shape, kernel_shape, stride, padding)
+    ii, jj, kk = get_neighborhood_indeces((B,C,H,W), kernel_shape, stride, padding)
     # get the entries of the input array at the selected indeces
     # neigh_index --> [kk, ii, jj] # indexing as BxCxHxW
     transformed_input = input[:, kk, ii, jj]
@@ -94,7 +103,7 @@ def image2neighbourhoodMatrix(input: np.ndarray, kernel_shape:tuple, stride:int,
     return transformed_input
 
 def neighborhoodMatrix2image(transformed_input:np.ndarray, input_shape:tuple, kernel_shape:tuple,
-                             stride:int, padding:int) -> tuple:
+                             stride:int, padding:int) -> np.ndarray:
 
     '''
     basically an inverse function to image2neighbourhoodMatrix(), where we transform the input back to its
@@ -118,15 +127,15 @@ def neighborhoodMatrix2image(transformed_input:np.ndarray, input_shape:tuple, ke
     H_padded, W_padded = H + 2 * padding, W + 2 * padding
     input_padded = np.zeros((B, C, H_padded, W_padded), dtype=transformed_input.dtype)
 
-    # we then used a particular set of indeces to transform 4D input into 2D matrix whith each neighborhood
+    # we then used a particular set of indices to transform 4D input into 2D matrix with each neighborhood
     # in each column, recover that
     ii, jj, kk = get_neighborhood_indeces(input_shape, kernel_shape, stride, padding)
 
     # reshape transformed_input so that feature maps are in a separate dimension
-    input = transformed_input.reshape(C * Hk * Wk, -1, Nk).transpose(2, 0, 1)
+    input = transformed_input.reshape(C * Hk * Wk, -1, B).transpose(2, 0, 1)
 
     # populate the input image with the corresponding entries
-    np.add.at(input_padded, (slice(None), kk, ii, jj), input)
+    np.add.at(input_padded, (slice(B), kk, ii, jj), input)
 
     # we only need the gradients for the original image, hence discard padded pixels if any:
     if padding != 0:
@@ -136,10 +145,11 @@ def neighborhoodMatrix2image(transformed_input:np.ndarray, input_shape:tuple, ke
 
     return input
 
-def correlation2d(input:np.ndarray, kernel:np.ndarray, bias:np.ndarray, stride:int = 1, padding:int = 0) -> np.ndarray:
+def correlation2d(input:np.ndarray, kernel:np.ndarray, bias:np.ndarray, stride:int = 1, padding:int = 0) -> tuple:
 
     '''
-    basic 2d correlation operator in1*in2. simply move throughout in1 and evaluate the local dot product with in2.
+    fast 2D correlation as matrix multiplication, see paper:
+    https://www.researchgate.net/publication/330315719_A_Uniform_Architecture_Design_for_Accelerating_2D_and_3D_CNNs_on_FPGAs
 
 
     :param in1: 4D np.ndarray, second input for the correlation operator. shape: BxCxHxW.
@@ -163,33 +173,18 @@ def correlation2d(input:np.ndarray, kernel:np.ndarray, bias:np.ndarray, stride:i
                 'input shape: {} filter shape: {}'.format(input.shape[-2:], kernel.shape[-2:]))
 
     # get output dimensions
-    Hout = int((H - Hk + 2 * padding)/stride) + 1
-    Wout = int((W - Wk + 2 * padding)/stride) + 1
-
-    # 3 pad image if necessary
-    # np.pad defaults to zero padding, do not pad in the channnel dimension
-    if padding>0:
-        input = np.pad(input, ((0,0), (0,0), (padding,padding), (padding, padding)), mode="constant")
+    Hout = int(np.floor((H - Hk + 2 * padding) / stride + 1))
+    Wout = int(np.floor((W - Wk + 2 * padding) / stride + 1))
 
     # perform convolution as matrix multpilication, and add bias of each kernel
     transformed_kernel = kernel.reshape(Nk, -1) # reshape kernels from Nk x Ck x Hk x Wk to Nk x Ck*Hk*Wk
-    transformed_input = image2neighbourhoodMatrix(input, kernel, stride, padding) # reshape input from B x C x H x W to (Ck*Hk*Wk)x(B*H*W)
+    transformed_input = image2neighbourhoodMatrix(input, kernel.shape, stride, padding) # reshape input from B x C x H x W to (Ck*Hk*Wk)x(B*H*W)
     out = transformed_kernel @ transformed_input + bias
 
     # reshape to proper form
-    out = out.reshape(Nk, Hout, Wout, B)
+    out = out.reshape(Nk, Hout, Wout, B).transpose(3,0,1,2)
 
-    # return cache to compute derivatives in backward pass (no need to store transformed_kernel as it is fastly computed)
-    cache = input, transformed_input, kernel, bias, stride, padding
+    # return cache to compute derivatives in backward pass
+    cache = {"input": input, "transformed_input":transformed_input}
 
     return out, cache
-
-def relu(input):
-    mask = input <= 0
-    input[mask] = 0
-    # grad = np.ones_like(input)
-    # grad[mask] = 0
-    return input
-
-def sigmoid(input):
-    return 1/(1+np.exp(-input))
